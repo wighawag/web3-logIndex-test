6f279bc52679ac4541e3ea2391384f8092be49b5
Ronan <wighawag@gmail.com>
2018-05-25 11:43:27 +0100

    use web3 for deploy too

--- test/test.js
+++ test/test.js
@@ -1,35 +1,40 @@
 const Web3 = require('web3');
 
-const Test = artifacts.require("Test");
+const TestArtifact = artifacts.require("Test");
+const TestContractInfo = TestArtifact._json;
 const web3 = new Web3();
-web3.setProvider(Test.web3.currentProvider);
+web3.setProvider(TestArtifact.web3.currentProvider);
+
+const Test = new web3.eth.Contract(TestContractInfo.abi, {data: TestContractInfo.bytecode});
+
 
 contract('Test', function(accounts) {
-  
+
   it("should result in 4 events whose logIndex represent the position in the transaction (and not as web3/json rpc doc states, the position in the block)", function() {
-    let test;
-    return Test.deployed().then(function(instance) {
-        test = new web3.eth.Contract(Test._json.abi,instance.address);
+    return Test.deploy().send({from:accounts[0], gas: 400000})
+        .then(function(test) {
+            return stopAutoMine()
+                .then(() => testEvents(test, accounts[0]))
+                .then(() => testEvents(test, accounts[0]))
+                .then(() => mine())
+                .then(() => test.getPastEvents("allEvents"));
+        })
+        .then(function(result) {
+            console.log(result);
+            assert.equal(result[0].logIndex, 0);
+            assert.equal(result[0].transactionIndex, 0);
 
-        return stopAutoMine()
-            .then(() => testEvents(test, accounts[0]))
-            .then(() => testEvents(test, accounts[0]))
-            .then(() => mine())
-            .then(() => test.getPastEvents("allEvents"));
-    }).then(function(result) {
-        assert.equal(result[0].logIndex, 0);
-        assert.equal(result[0].transactionIndex, 0);
+            assert.equal(result[1].logIndex, 1);
+            assert.equal(result[1].transactionIndex, 0);
 
-        assert.equal(result[1].logIndex, 1);
-        assert.equal(result[1].transactionIndex, 0);
+            assert.equal(result[2].logIndex, 0);
+            assert.equal(result[2].transactionIndex, 1);
 
-        assert.equal(result[2].logIndex, 0);
-        assert.equal(result[2].transactionIndex, 1);
-
-        assert.equal(result[3].logIndex, 1);
-        assert.equal(result[3].transactionIndex, 1);
-    });
+            assert.equal(result[3].logIndex, 1);
+            assert.equal(result[3].transactionIndex, 1);
+        });
   });
+
 });
 
 function testEvents(testContract, from) {
@@ -40,6 +45,7 @@ function testEvents(testContract, from) {
         });
 
         promiEvent.once('transactionHash', (txHash) => {
+            console.log(txHash);
             resolve(txHash);
         });
     });
